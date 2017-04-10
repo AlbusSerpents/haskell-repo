c89d627fbe86b4b43c3b130ca4d5a0259708e182
AlbusSerpents <naidim@abv.bg>
2017-04-10 22:55:59 +0300

    Initial commit

---
+++ Utils.hs
@@ -0,0 +1,12 @@
+module Utils
+(
+	clear
+)
+where
+{-# LANGUAGE TypeSynonymInstances, OverlappingInstances #-}
+
+import System.IO
+import System.Process
+import System.Random
+
+clear = system "cls"
---
+++ pattern-matcher.hs
@@ -0,0 +1,17 @@
+module GlobRegex
+(
+	globToRegex,
+	matchesRegex
+)
+where 
+
+import qualified Text.Regex.Posix ((=~))
+import Utils
+
+globToRegex :: String -> String
+globToRegex _ = ""
+
+matchesRegex = ""
+
+
+
