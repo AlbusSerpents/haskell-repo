54666af11ba0adcf869f606ef43083d7e58965a6
AlbusSerpents <naidim@abv.bg>
2017-04-12 17:28:54 +0300

    added helper method

--- pattern-matcher.hs
+++ pattern-matcher.hs
@@ -9,10 +9,20 @@ import qualified Text.Regex.Posix ((=~))
 import Utils
 
 globToRegex :: String -> String
-globToRegex _ = undefined
+globToRegex cs = '^' : globToRegexHelper cs ++ "$"
 
 matchesRegex :: String -> String -> Bool
 matchesRegex _ _ = undefined
 
+globToRegexHelper :: String -> String
+globToRegexHelper "" = ""
 
+globToRegexHelper ('*':cs) = ".*" ++ globToRegexHelper cs
+globToRegexHelper ('?':cs) = "." ++ globToRegexHelper cs
+
+globToRegexHelper ('[':'!':c:cs) = "[^" ++ c : charClass cs
+globToRegexHelper ('[':c:cs) = "[" ++ c : charClass cs
+globToRegexHelper ('[':_) = error "unterminated character class"
+
+globToRegexHelper (c:cs) = escape c ++ globToRegexHelper cs
 
