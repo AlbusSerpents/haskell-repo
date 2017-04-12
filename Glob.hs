aca07e224275716208edce02b92069e6dc7d7d36
AlbusSerpents <naidim@abv.bg>
2017-04-12 20:55:57 +0300

    isPattern function implemented

--- Glob.hs
+++ Glob.hs
@@ -5,3 +5,24 @@ module Glob
 
 where 
 
+import System.Directory (doesDirectoryExist, doesFileExist,
+	getCurrentDirectory, getDirectoryContents)
+import System.FilePath (dropTrailingPathSeparator, 
+	splitFileName, (</>))
+import Utils
+import GlobRegex (matchesRegex, unterminatedCharClassMessage)
+import Control.Exception (handle)
+import Control.Monad (forM)
+
+namesMatching :: String -> IO [String]
+namesMatching pattern 
+	| not $ isPattern pattern = do
+		exists <- doesNameExist pattern
+		return (if exists then [pattern] else [])
+
+isPattern :: String -> Bool
+isPattern text = any (\e -> e `elem` patternSymbols) text
+
+patternSymbols = "?[*"
+
+doesNameExist = undefined
