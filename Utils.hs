ac7affd9803aada49dee302bda5a57bb7fb4125b
AlbusSerpents <naidim@abv.bg>
2017-04-17 01:03:45 +0300

    implementation of splitOn. TODO fix issues with the literal appearing on the splits

--- Utils.hs
+++ Utils.hs
@@ -1,6 +1,7 @@
 module Utils
 (
-	clear
+	clear,
+        splitOn
 )
 where
 {-# LANGUAGE TypeSynonymInstances, OverlappingInstances #-}
@@ -10,3 +11,19 @@ import System.Process
 import System.Random
 
 clear = system "clear"
+
+splitOn :: String -> String -> [String]
+splitOn src symbols = 
+                    splitHelper [] src
+                     where
+                        len = length symbols
+
+                        splitHelper :: [String] -> String -> [String]
+                        splitHelper res [] = reverse (map reverse res)
+                        splitHelper [] (c:cs) = splitHelper [[c]] cs
+                        splitHelper (curr:ready) str 
+                                        | length str < length symbols = splitHelper ((str++curr):ready) []
+                                        | cut /= symbols = let lead = ((head str):curr)
+                                                           in splitHelper (lead:ready) (tail str)
+                                        | cut == symbols = splitHelper (cut:(curr:ready)) (drop len str)
+                                        where cut = take len str
