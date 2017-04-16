03dbe235693e153c384f652993d6fbc349f5ec49
AlbusSerpents <naidim@abv.bg>
2017-04-16 20:52:33 +0300

    Implemented the module

--- FileTree.hs
+++ FileTree.hs
@@ -4,15 +4,27 @@ module FileTree
 )
 where 
 
-import System.FilePath ((</>))
+import System.FilePath ((</>), addTrailingPathSeparator)
 import System.Posix (getFileStatus, isDirectory)
-import Control.Monad (forM)
+import Control.Monad (forM, mapM)
 import System.Directory (getDirectoryContents)
+import Data.List (delete)
 
-data State = File | Directory
+data FileState = File | Directory
 
 allChildFiles :: FilePath ->  IO [FilePath]
-allChildFiles file = undefined
+allChildFiles "./" = return ([])
+allChildFiles "../" = return ([])
+allChildFiles filePath = do 
+        results <- getLayer filePath
+        execute results
+        where 
+                execute (files, []) = do return (map (filePath </>) files)
+                execute (files, dirs) = do
+                                sub <- mapM allChildFiles dirs
+                                return ((map (filePath </>) files) ++ concat sub)  
+
+selfRelatingDirectories = [".",".."]
 
 isDir :: FilePath -> IO Bool
 isDir path = do
@@ -22,15 +34,18 @@ isDir path = do
 getLayer :: FilePath -> IO ([FilePath],[FilePath])
 getLayer path = do
         contents <- getDirectoryContents path
-        marked <- forM contents (\fp -> do 
-                        status <- isDir (path </> fp)
-                        if status 
-                                then return(fp, Directory)
-                                else return(fp, File)                        
-                      )
+        marked <- forM (filter (\e -> e `notElem` selfRelatingDirectories) contents) (assignFileState path)
         return (foldr foldHelper ([],[]) marked)
 
-foldHelper :: (FilePath, State) -> ([FilePath], [FilePath]) -> ([FilePath], [FilePath]) 
+assignFileState :: FilePath -> FilePath -> IO (FilePath, FileState)
+assignFileState source path =  do
+        status <- isDir (fullPath)
+        if status 
+                then return(addTrailingPathSeparator fullPath, Directory)
+                else return(path, File)
+        where fullPath = source </> path
+
+foldHelper :: (FilePath, FileState) -> ([FilePath], [FilePath]) -> ([FilePath], [FilePath]) 
 foldHelper (path, File) (files, dirs) = (path : files, dirs)
 foldHelper (path, Directory) (files, dirs) = (files, path : dirs)
 
